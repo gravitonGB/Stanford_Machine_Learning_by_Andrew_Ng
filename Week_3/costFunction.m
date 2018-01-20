c418e8917ab87c1c53fb15e0a4df5b1d6eef4e11
gravitonGB <31953056+gravitonGB@users.noreply.github.com>
2018-01-20 16:47:00 +0300

    Add files via upload

---
+++ Week_3/costFunction.m
@@ -0,0 +1,42 @@
+function [J, grad] = costFunction(theta, X, y)
+%COSTFUNCTION Compute cost and gradient for logistic regression
+%   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+%   parameter for logistic regression and the gradient of the cost
+%   w.r.t. to the parameters.
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Note: grad should have the same dimensions as theta
+%
+z = X * theta;
+[h] = sigmoid(z); 
+J = 1 / m * (-y'*log(h) - (1 - y)' * log(1 - h));
+grad = 1 / m * X' * (h - y);
+
+% GB 
+% for j = 1:size(X,2)
+%    S1 = 0;
+%    S2 = 0;
+%    for i = 1:size(X,1)
+%        z(i) = X(i,:) * theta;
+%        [h(i)] = sigmoid(z(i));
+%        S1 = S1 + (h(i) - y(i)) * X(i,j);
+%        S2 = S2 + (-y(i) * log(h(i)) - (1-y(i)) * log(1 - h(i)));
+%    end
+%    grad(j) = 1 / m * S1;
+%    J(j) = 1 / m * S2;
+%end
+% =============================================================
+
+end
